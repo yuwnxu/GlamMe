6fee61e6a3b416293b2c183521dd6d8292c461eb
yuwnxu <yulduzgebekova@yandex.ru>
2026-03-12 19:24:25 +0500

    Создан экран Профиль

--- lib/presentation/mainScreens/sections/profile.dart
+++ lib/presentation/mainScreens/sections/profile.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:glamme/presentation/uikit/colors.dart';
+import 'package:glamme/presentation/uikit/custom_textfield.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../domain/globals.dart';
+import '../../uikit/custom_button.dart';
 
 // Экран Profile
 class Profile extends StatefulWidget {
@@ -9,8 +16,112 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  void initState() {
+    super.initState();
+    emailController.addListener(updateWidget);
+    passwordController.addListener(updateWidget);
+    nameController.addListener(updateWidget);
+  }
+
+  void updateWidget() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.removeListener(updateWidget);
+    passwordController.removeListener(updateWidget);
+    nameController.removeListener(updateWidget);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: background,
+      body: SafeArea(child:
+      Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: Row(
+              children: [
+                SvgPicture.asset('assets/icons/arrow.svg'),
+                Spacer(),
+                Text('Профиль', style: GoogleFonts.sulphurPoint(fontSize: 16, fontWeight: FontWeight.bold, color: title),),
+                Spacer(),
+              ],
+            ),
+          ),
+          SizedBox(height: 50,),
+          Center(child: Image.asset('assets/images/user_icon.png')),
+          SizedBox(height: 45,),
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              children: [
+                CustomTextField(
+                  type: TextFieldType.text,
+                  controller: nameController,
+                  colorBorder: border,
+                  colorFocus: black,
+                  colorError: error,
+                  title: 'Имя',
+                  hintText: '',
+                  errorText: nameError,
+                  borderRadius: 15,
+                  colorCursor: black,
+                ),
+                SizedBox(height: 20),
+                CustomTextField(
+                  type: TextFieldType.text,
+                  controller: emailController,
+                  colorBorder: border,
+                  colorFocus: black,
+                  colorError: error,
+                  title: 'Почта',
+                  hintText: 'name@mail.com',
+                  errorText: emailError,
+                  borderRadius: 15,
+                  colorCursor: black,
+                ),
+                SizedBox(height: 20),
+                CustomTextField(
+                  type: TextFieldType.password,
+                  controller: passwordController,
+                  colorBorder: border,
+                  colorFocus: black,
+                  colorError: error,
+                  title: 'Пароль',
+                  hintText: '**********',
+                  errorText: passwordError,
+                  borderRadius: 15,
+                  colorCursor: black,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 30),
+          CustomButton(
+            textButton: 'Изменить',
+            colorButton: black,
+            widthButton: 352,
+            heightButton: 56,
+            borderRadius: 20,
+            onButton: () {
+              if (emailError == null ||
+                  passwordError == null ||
+                  nameError == null) {
+                navToProfile(context);
+              } else {
+                if (mounted) {
+                  setState(() {});
+                }
+              }
+            },
+          ),
+        ],
+      )),
+    );
   }
 }
--- lib/presentation/uikit/colors.dart
+++ lib/presentation/uikit/colors.dart
@@ -9,3 +9,4 @@ Color hintText = Color(0xff696969);
 Color border = Color(0xffD1D1D1);
 Color black = Color(0xff151515);
 Color error = Color(0xffEF3030);
+Color title =  Color(0xFF504949);
